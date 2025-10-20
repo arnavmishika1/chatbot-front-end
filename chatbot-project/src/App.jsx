96721bebb6910455c9214d0b462b3d452926b3cb
arnavmishika1 <arnavmishika1@gmail.com>
2025-10-20 17:40:24 +0530

    Save to localstorage

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -5,28 +5,30 @@ import ChatMessages from './components/ChatMessages';
 import './App.css'
 
 function App() {
-  const [ chatMessages, setChatMessages ] = useState([{
-    message: 'hello chatbot',
-    sender: 'user',
-    id: 'id1',
-    time: 1736127288920
-  }, {
-    message: 'Hello! How can I help you?',
-    sender: 'robot',
-    id: 'id2',
-    time: 1736127291230
-  }, {
-    message: 'can you get me todays date?',
-    sender: 'user',
-    id: 'id3',
-    time: 1736127385356
-  }, {
-    message: 'Today is October 14',
-    sender: 'robot',
-    id: 'id4',
-    time: 1736127385500
-  }]);
-  // const [ chatMessages, setChatMessages ] = useState([]);
+  // const [ chatMessages, setChatMessages ] = React.useState([{
+  //   message: 'hello chatbot',
+  //   sender: 'user',
+  //   id: 'id1',
+  //   time: 1736127288920
+  // }, {
+  //   message: 'Hello! How can I help you?',
+  //   sender: 'robot',
+  //   id: 'id2',
+  //   time: 1736127291230
+  // }, {
+  //   message: 'can you get me todays date?',
+  //   sender: 'user',
+  //   id: 'id3',
+  //   time: 1736127385356
+  // }, {
+  //   message: 'Today is October 14',
+  //   sender: 'robot',
+  //   id: 'id4',
+  //   time: 1736127385500
+  // }]);
+  const [ chatMessages, setChatMessages ] = useState(
+    JSON.parse(localStorage.getItem('messages')) || []
+  );
   // const [ chatMessages, setChatMessages ] = array;
   // const chatMessages = array[0];
   // const setChatMessages = array[1];
@@ -43,6 +45,11 @@ function App() {
     // this setup code once because we only want to add these
     // extra responses once.
   }, []);
+
+  useEffect(() => {
+    // save to local storage
+    localStorage.setItem('messages', JSON.stringify(chatMessages));
+  }), [chatMessages];
 
   return (
     <div className="app-container">
