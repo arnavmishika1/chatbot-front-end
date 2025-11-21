659dbae527cacc7609f1d7da6c1cb697ade2bf57
arnavmishika1 <arnavmishika1@gmail.com>
2025-11-21 13:13:55 +0530

    Convert ecommerce project to TypeScript

---
+++ old-projects/ecommerce-project-js/src/pages/home/HomePage.jsx
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router';
+import { Header } from "../../components/Header";
+import { ProductGrid } from './ProductsGrid';
+import "./HomePage.css";
+
+export function HomePage({ cart, loadCart }) {
+  const [products, setProducts] = useState([]);
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get('search');
+
+  useEffect(() => {
+    const fetchHomeData = async () => {
+      const urlPath = search ? `/api/products?search=${search}` : '/api/products';
+      const response = await axios.get(urlPath);
+      setProducts(response.data);
+    }
+    
+    fetchHomeData();
+  }, [search]);
+  
+  return (
+    <>
+      <title>Ecommerce Project</title>
+      <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
+
+      <Header cart={cart} />
+
+      <div className="home-page">
+        <ProductGrid products={products} loadCart={loadCart} />
+      </div>
+    </>
+  );
+}
---
+++ old-projects/ecommerce-project-js/src/pages/orders/OrdersGrid.jsx
@@ -0,0 +1,53 @@
+import dayjs from "dayjs";
+import axios from "axios";
+import { Fragment } from 'react';
+import { Link } from "react-router";
+import BuyAgainIcon from "../../assets/images/icons/buy-again.png";
+
+export function OrdersGrid({ order, loadCart }) {  
+  return (
+    <div className="order-details-grid">
+      {order.products.map((orderProduct) => {
+        const addToCart = async () => {
+          await axios.post('api/cart-items', {
+            productId: orderProduct.productId,
+            quantity: 1
+          });
+          await loadCart();
+        };
+
+        return (
+          <Fragment key={orderProduct.productId}>
+            <div className="product-image-container">
+              <img src={orderProduct.product.image} />
+            </div>
+
+            <div className="product-details">
+              <div className="product-name">{orderProduct.product.name}</div>
+              <div className="product-delivery-date">
+                Arriving on:{" "}
+                {dayjs(orderProduct.estimatedDeliveryTimeMs).format("MMMM D")}
+              </div>
+              <div className="product-quantity">
+                Quantity: {orderProduct.quantity}
+              </div>
+              <button className="buy-again-button button-primary"
+                onClick={addToCart}>
+                <img className="buy-again-icon" src={BuyAgainIcon} />
+                <span className="buy-again-message">Add to Cart</span>
+              </button>
+            </div>
+
+            <div className="product-actions">
+              <Link to={`/tracking/${order.id}/${orderProduct.productId}`}>
+                <button className="track-package-button button-secondary">
+                  Track package
+                </button>
+              </Link>
+            </div>
+          </Fragment>
+        );
+      })}
+    </div>
+  );
+}
